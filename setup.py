e89931dd5b8062e6342cff6f332a2634853ac13a
Davis Weaver <dweav94@gmail.com>
2022-02-07 11:52:11 -0500

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='evodm', 
-    version='0.1.0', 
+    version='1.0.0', 
     author = 'Davis Weaver', 
     author_email = 'dtw43@case.edu',
     packages=['evodm', "evodm.test"], 

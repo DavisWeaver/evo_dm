dba1c39a76a7fd679d4f3b40e455544ea4c42cca
Weaver <WEAVERD3@ccf.org>
2022-10-04 14:28:13 -0400

    fix init

--- evodm/__init__.py
+++ evodm/__init__.py
@@ -1,7 +1,7 @@
 from .learner import hyperparameters, DrugSelector, practice
 from .landscapes import Landscape
 from .evol_game import evol_env, generate_landscapes, define_drugs, normalize_landscapes, run_sim 
-from .exp import evol_deepmind, define_mira_landscapes, mdp_mira_sweep, policy_sweep, test_generic_policy, sweep_replicate_policy, mdp_sweep
+from .exp import evol_deepmind, define_mira_landscapes, mdp_mira_sweep, policy_sweep, test_generic_policy, sweep_replicate_policy, mdp_sweep, signal2noise
 __all__ = [
     'evol_deepmind',
     'hyperparameters', 
@@ -18,5 +18,6 @@ __all__ = [
     'mdp_mira_sweep',
     'policy_sweep', 
     'test_generic_policy', 
-    'sweep_replicate_policy'
+    'sweep_replicate_policy',
+    'signal2noise'
 ]

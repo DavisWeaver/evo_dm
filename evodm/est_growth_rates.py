598bef6570400dcde3ce02e104986856b9442e59
King <kinge2@lri-108705.cc.ad.cchs.net>
2023-01-09 10:43:33 -0500

    updated est growth rates to include previous drug action in output.

--- evodm/est_growth_rates.py
+++ evodm/est_growth_rates.py
@@ -53,71 +53,99 @@ def parse_data_file(df):
 
     return df_filt
 
-df = pd.read_excel(data_path)
+def est_growth_rates(data_path,prev_action,save_folder) -> dict:
+    """Takes in optical density data and returns growth rates
 
-data = parse_data_file(df)
+    Time series optical density data in a 96-well plate -> a dict of output
+    arrays. The output array for a given well is an array of size 16 with the 
+    first 15 entries representing the drug action of the previous day and the 
+    last entry representing the estimated growth rate.
 
-row_list = ['A','B','C','D','E','F','G','H']
+    Also generates a figure for each curve fit and saves the figures to a
+    folder.
 
-results = {}
+    Args:
+        data_path (str): .csv or .xlsx file with OD data
+        prev_policy (dict): Dict of drugs used on the previous day for each well
+        save_folder (str): Folder to save images of fits for spot-checking
+    """
 
-# Create the save folder
-if not os.path.exists(save_folder):
-    os.mkdir(save_folder)
+    df = pd.read_excel(data_path)
 
-for row in row_list:
-    for col in range(1,13):
+    data = parse_data_file(df)
 
-        fig,ax_list = plt.subplots(ncols=2,figsize=(8,3))
+    row_list = ['A','B','C','D','E','F','G','H']
 
-        key = row + str(col)
+    results = {}
 
-        OD = np.array(data[key]).astype(float)
-        time = np.array(data['Time [s]']).astype(float)
+    # Create the save folder
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
 
-        if OD[0] > OD[1]:
-            OD = OD[1:]
-            time = time[1:]
+    for row in row_list:
+        for col in range(1,13):
+
+            fig,ax_list = plt.subplots(ncols=2,figsize=(8,3))
+
+            key = row + str(col)
+
+            OD = np.array(data[key]).astype(float)
+            time = np.array(data['Time [s]']).astype(float)
+
+            if OD[0] > OD[1]:
+                OD = OD[1:]
+                time = time[1:]
 
 
-        lnOD = np.log(OD/np.min(OD))
+            lnOD = np.log(OD/np.min(OD))
 
-        # Get the break points using pelt
-        algo = rpt.Pelt(model='l1').fit(lnOD)
-        breaks = algo.predict(pen=2)
+            # Get the break points using pelt
+            # algo = rpt.Pelt(model='l1').fit(lnOD)
+            # breaks = algo.predict(pen=2)
+            algo = rpt.Pelt(model='l2',jump=2,min_size=5).fit(lnOD)
+            breaks = algo.predict(pen=0.1)
 
-        # plot the break points
-        break1 = time[[breaks[0],breaks[0]]]
-        break2 = time[[breaks[1],breaks[1]]]
-        y = [lnOD[0],np.max(lnOD)]
+            # plot the break points
+            break1 = time[[breaks[0],breaks[0]]]
+            break2 = time[[breaks[1],breaks[1]]]
+            y = [lnOD[0],np.max(lnOD)]
 
-        ax = ax_list[0]
+            ax = ax_list[0]
 
-        ax.plot(time,lnOD,linewidth=2)
-        ax.plot(break1,y,linewidth=2)
-        ax.plot(break2,y,linewidth=2)
+            ax.plot(time,lnOD,linewidth=2)
+            ax.plot(break1,y,linewidth=2)
+            ax.plot(break2,y,linewidth=2)
 
-        # Get the linear segment of data determined by the break points
-        lin_seg = lnOD[breaks[0]:breaks[1]]
-        time_t = np.array(time[breaks[0]:breaks[1]])
+            # Get the linear segment of data determined by the break points
+            lin_seg = lnOD[breaks[0]:breaks[1]]
+            time_t = np.array(time[breaks[0]:breaks[1]])
 
-        # linear regression to get the slope
+            # linear regression to get the slope
 
-        res = scipy.stats.linregress(time_t,lin_seg)
+            res = scipy.stats.linregress(time_t,lin_seg)
 
-        fit = time_t*res.slope + res.intercept
+            fit = time_t*res.slope + res.intercept
 
-        ax = ax_list[1]
+            ax = ax_list[1]
 
-        # plot the linear regression
-        ax.plot(time_t,lin_seg,linewidth=2)
-        ax.plot(time_t,fit,linewidth=2)
+            # plot the linear regression
+            ax.plot(time_t,lin_seg,linewidth=2)
+            ax.plot(time_t,fit,linewidth=2)
 
-        # add the results to the results dict
-        results[key] = res.slope
+            # add the results to the results dict
 
-        ax.set_title(key)
+            # output is an array representing the previous day's drug concatenated
+            # with the growth rate
+            output = np.zeros(16)
+            output[prev_action[key]] = 1
+            output[-1] = res.slope
 
-        # save the figure for spot checking
-        fig.savefig(save_folder + os.sep + key + '.png',bbox_inches='tight')
-        plt.close(fig)
+            results[key] = output
+
+            ax.set_title(key)
+
+            # save the figure for spot checking
+            fig.savefig(save_folder + os.sep + key + '.png',bbox_inches='tight')
+            plt.close(fig)
+    
+    return results

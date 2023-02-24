c5416ca157e556db64383b330615e81bb863e0ca
Weaver <WEAVERD3@ccf.org>
2023-02-24 16:02:03 -0500

    added n adaptive jumps to the calculation of allowable mutants

--- evodm/landscapes.py
+++ evodm/landscapes.py
@@ -8,6 +8,9 @@ import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def flatten(list_of_lists):
+    return list(itertools.chain.from_iterable(list_of_lists))
+
 class Landscape:
     """
     This class represents Landscapes which are used as the central objects for Markov evolutions and other calculations.
@@ -54,13 +57,14 @@ class Landscape:
     get_steadystate_rounds(correl)
         Calculates number of steps to reach steady state for paired landscape evolution
     """
-    def __init__(self, N, sigma, ls=None, parent=None):
+    def __init__(self, N, sigma, ls=None, parent=None, num_jumps = 1):
         """
         Initializes landscape objects with given N and sigma to simulate epistasis (zero sigma produces an additive landscape with exactly one global maximum).
         """
         self.N = N
         self.sigma = sigma
         self.Bs = None
+        self.num_jumps = num_jumps
         if ls is None:
             self.ls = np.array([0]) # Initializes landscape vector with fitness 0 in the first (wildtype) index
             fitness = np.random.uniform(-1, 1, N) # Generates N fitness values between -1 and 1 that will be used to generate the additive landscape.
@@ -88,8 +92,8 @@ class Landscape:
             TM = np.zeros((2**self.N,2**self.N))                                   # Transition matrix will be sparse (most genotypes unaccessible in one step) so initializes a TM with mostly 0s to do most work for us.
 
             for i in range(2**self.N):
-                adjMut = [i ^ (1 << m) for m in mut]                     # For the current genotype i, creates list of genotypes that are 1 mutation away.
-
+                                     # For the current genotype i, creates list of genotypes that are 1 mutation away.
+                adjMut = self.define_adjMut(mut = mut, i = i) #This function implements new possibilities for HGT
                 adjFit = [self.ls[j] for j in adjMut]                         # Creates list of fitnesses for each corresponding genotype that is 1 mutation away.
 
                 fitter = list(filter(lambda x: (adjFit[x]-self.ls[i]) > 0.00001, mut))                      # Finds which indices of adjFit are more fit than the current genotype and thus available for mutation.
@@ -104,6 +108,40 @@ class Landscape:
             if store: self.TM = TM # store the transition matrix for this landscape object
             return TM
         else: return self.TM
+
+    def define_adjMut(self, mut, i):
+        """
+        define the allowable adjacent mutants
+
+        Get rid of rounding issues..
+
+        old code:  fitter = list(filter(lambda x: adjFit[x]>self.ls[i], mut))
+
+        new code:  fitter = list(filter(lambda x: (adjFit[x]-self.ls[i]) > 0.00001, mut))
+        """
+        adjMut = [i ^ (1 << m) for m in mut]
+        if self.num_jumps > 1:
+            extra_edges=[] #The rules - no gain of allele and loss of allele on same jump - only multi-gains and multi-losses
+            for jumps in range(2, self.num_jumps+1): #self.num_jumps + 1 because of pythons stupid dumb 0 indexing scheme that I hate
+                for j in range(2**self.N):
+                    if j == i:
+                        continue
+                    elif bin(j).count("1") == (bin(i).count("1") + jumps): #check if test_mut is an allowable forward transition
+                        extra_edges.append(j)
+                    elif bin(j).count("1") == (bin(i).count("1") + jumps): #some condition for backward double losses
+                        extra_edges.append(j)
+                    else:
+                        continue
+
+        for t in iter(extra_edges):
+            adjMut.append(t)
+        
+        adjMut = [*set(adjMut)]
+
+        return adjMut
+        
+            
+
 
     def get_TM_phenom(self, phenom, store=False):
         """

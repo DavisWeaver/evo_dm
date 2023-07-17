0a485ed33e43004fc7c07f0f63e5d422b7469c47
Davis Weaver <dweav94@gmail.com>
2023-07-17 10:13:07 -0400

    tensorflow version issue hotfix

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author_email = 'dtw43@case.edu',
     packages=['evodm', "evodm.test"], 
     install_requires = [
+      "protobuf~=3.20.3",
       "tensorflow~=2.12.0", 
-      "keras~=2.12.0",
       "numpy",
       "tqdm",
       "scipy", 

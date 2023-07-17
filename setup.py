30420b8e8310714f29cf6d8ecfcdeef4ba63fc29
Davis Weaver <dweav94@gmail.com>
2023-07-17 17:01:52 -0400

    get rid of protobuf requirement

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email = 'dtw43@case.edu',
     packages=['evodm', "evodm.test"], 
     install_requires = [
-      "protobuf~=3.19.6",
       "tensorflow~=2.11.0", 
       "numpy",
       "scipy", 
